f1cb657b0a0fdd0e3f3a22e29f0b697301344640
eldorahmed <eldorbek970304@gmail.com>
2024-04-21 16:41:37 +0500

    homework added

--- js/app.js
+++ js/app.js
@@ -95,6 +95,11 @@ const projects = [
     githubLink: "https://github.com/eldorahmed/2-dars-String-Methods",
     vercelLink: "https://2-dars-string-methods.vercel.app/",
   },
+  {
+    title: "3-dars misollar ",
+    githubLink: "https://github.com/eldorahmed/3-dars",
+    vercelLink: "https://3-lesson-sand.vercel.app/",
+  },
 ];
 
 const template = document.querySelector("template");
